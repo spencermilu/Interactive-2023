f7202b7e34a9820f115cb2523a7d7bde5bc4c628
spencermilu <siming.qiao@cca.edu>
2023-05-07 11:56:44 -0700

    1

---
+++ Exercises/Exercise-11/script.js
@@ -0,0 +1,21 @@
+function updateClock() {
+    const hourHand = document.querySelector('.hour-hand');
+    const minuteHand = document.querySelector('.minute-hand');
+    const secondHand = document.querySelector('.second-hand');
+
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const minutesDegrees = ((minutes / 60) * 360) + 90;
+    const hoursDegrees = (((hours % 12) + (minutes / 60)) / 12) * 360 + 90;
+
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
